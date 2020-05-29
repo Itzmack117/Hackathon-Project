91493193201e0b9c3d1b2ee4a48d125c80f0a702
micahjbartron <micahjbartron@gmail.com>
2020-05-29 16:47:03 -0600

    commiting all my things

--- clientsrc/app/Controllers/PostController.js
+++ clientsrc/app/Controllers/PostController.js
@@ -6,7 +6,7 @@ function _draw() {
   let post = store.State.posts;
   let template = ""
   post.forEach(p => template += p.Template)
-  //NOTE ADD TEMPLATE TO DOCUMENT AND THAT SORT OF CRAP
+  document.getElementById("posts").innerHTML = template
 }
 
 function _drawApiPosts() {
@@ -18,9 +18,23 @@ export default class PostController {
   constructor() {
     store.subscribe("posts", _draw);
     this.getAllPosts()
+
   }
 
   getAllPosts() {
     service.getAllPosts()
+  }
+  createNewPost(event) {
+
+    event.preventDefault()
+    let formData = event.target
+    let rawData = {
+      user: formData.user.value,
+      title: formData.title.value,
+      body: formData.body.value,
+    }
+
+    service.createNewPost(rawData)
+    // formData.reset()
   }
 }
--- clientsrc/app/Services/PostService.js
+++ clientsrc/app/Services/PostService.js
@@ -7,6 +7,14 @@ const _postApi = axios.create({
 })
 
 class PostService {
+  async createNewPost(rawData) {
+    let data = await _postApi.post("", rawData)
+    this.getAllPosts()
+    console.log(data);
+
+
+
+  }
   async getAllPosts() {
     let data = await _postApi.get()
     let testDate = new Date(data.data[0].createdAt)
